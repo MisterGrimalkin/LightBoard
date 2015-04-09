dc961cf27043136e9c70bf9c9ed1f8e913f107ef
MisterGrimalkin <loki@amarantha.net>
2015-04-09 21:22:24 +0100

    Start mpeg support

--- src/main/java/lightboard/Main.java
+++ src/main/java/lightboard/Main.java
@@ -6,6 +6,9 @@ import lightboard.board.PolychromeLightBoard;
 import lightboard.board.impl.GraphicalBoard;
 import lightboard.board.impl.TextBoard;
 import lightboard.board.surface.LightBoardSurface;
+import lightboard.board.surface.MonochromeLightBoardSurface;
+import lightboard.board.surface.PolychromeLightBoardSurface;
+import lightboard.board.zone.impl.ImageZone;
 
 import static lightboard.board.zone.Zones.*;
 
@@ -24,17 +27,26 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
 
         int cols = COLS;
-        int rows = ROWS;
+        int rows = 200;
 
         PolychromeLightBoard board = new GraphicalBoard(rows, cols, primaryStage, "Travel Board", 2, 1).debugTo(new TextBoard(rows, cols));
         board.init();
 
-        LightBoardSurface surface = new LightBoardSurface(board);
+        LightBoardSurface surface = new PolychromeLightBoardSurface(board);
         surface.init();
 
-        startClock              (surface, COLS - CLOCK_WIDTH, 0,    CLOCK_WIDTH, ROWS);
+        ImageZone image = ImageZone.scrollUp(surface);
+        image.region(0,16,cols,rows-16);
+        image.loadImage("test-image.jpg");
+        image.start();
+
+
+        startClock(surface, COLS - CLOCK_WIDTH, 0, CLOCK_WIDTH, ROWS);
         startBusStopDisplay     (surface, 0, 0,                     COLS - CLOCK_WIDTH, ROWS/2);
         startTubeStatusDisplay  (surface, 0, ROWS/2,                COLS - CLOCK_WIDTH, ROWS/2,         "bad");
+
+//        new Thread(new VideoTest()).start();
+
 
     }
 
---
+++ src/main/java/lightboard/VideoTest.java
@@ -0,0 +1,34 @@
+package lightboard;
+
+/**
+ * Created by grimalkin on 09/04/15.
+ */
+public class VideoTest {//implements Runnable {
+
+//    private MarvinVideoInterface videoAdapter;
+//    private MarvinImage videoFrame;
+//
+//    public VideoTest(){
+//        try{
+//            // Create the VideoAdapter used to load the video file
+//            videoAdapter = new MarvinJavaCVAdapter();
+//            videoAdapter.loadResource("test-video.wmv");
+//
+//            // Start the thread for requesting the video frames
+//            new Thread(this).start();
+//        }
+//        catch(MarvinVideoInterfaceException e){e.printStackTrace();}
+//    }
+//
+//    @Override
+//    public void run() {
+//        try{
+//            while(true){
+//                // Request a video frame
+//                videoFrame = videoAdapter.getFrame();
+//                System.out.println("Hello");
+//            }
+//        }catch(MarvinVideoInterfaceException e){e.printStackTrace();}
+//    }
+//
+}
