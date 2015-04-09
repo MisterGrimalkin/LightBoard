09471dc0b837ffd3964743e4a27c6d6ff9143c72
MisterGrimalkin <loki@amarantha.net>
2015-04-09 15:39:31 +0100

    Jpeg support now working

--- src/main/java/lightboard/board/impl/GraphicalBoard.java
+++ src/main/java/lightboard/board/impl/GraphicalBoard.java
@@ -155,7 +155,7 @@ public class GraphicalBoard implements MonochromeLightBoard, PolychromeLightBoar
                 double red = data[0][r][c];
                 double green = data[1][r][c];
                 double blue = data[2][r][c];
-                if ( leds[c]!=null ) {
+                if ( leds[r][c]!=null ) {
                     leds[r][c].setFill(Color.color(red, green, blue));
                 }
             }
