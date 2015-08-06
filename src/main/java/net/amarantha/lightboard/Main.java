1adfe96fb4f467ca17798f1df400aa9a01a60d89
MisterGrimalkin <loki@amarantha.net>
2015-08-06 12:10:16 +0100

    Added Guice. Partially complete

--- src/main/java/net/amarantha/lightboard/Main.java
+++ src/main/java/net/amarantha/lightboard/Main.java
@@ -1,30 +1,20 @@
 package net.amarantha.lightboard;
 
-import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import net.amarantha.lightboard.board.LightBoard;
-import net.amarantha.lightboard.board.impl.GraphicalBoard;
 import net.amarantha.lightboard.module.ApplicationModule;
+import net.amarantha.lightboard.module.SimulationModule;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static com.google.inject.util.Modules.override;
 
 public class Main extends Application {
 
-    private static boolean simulationMode = false;
-    private static boolean selfTest = false;
-    private static boolean suppressWebService = false;
-
     public static void main(String[] args) {
 
-        List<String> params = Arrays.asList(args);
-        selfTest =              params.contains("selftest");
-        simulationMode =        params.contains("simulation");
-        suppressWebService =    params.contains("noserver");
+        boolean simulationMode = Arrays.asList(args).contains("simulation");
 
         if ( simulationMode ) {
             launch(args);
@@ -38,19 +28,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
         Guice.createInjector(
-                override(new ApplicationModule())
-                    .with(new AbstractModule() {
-                        @Override
-                        protected void configure() {
-                            bind(LightBoard.class).to(GraphicalBoard.class);
-                            bind(Stage.class).toInstance(primaryStage);
-                        }
-                    }))
-                .getInstance(LightBoardApplication.class)
-                    .startApplication();
-
+            override(
+                new ApplicationModule()).with(new SimulationModule(primaryStage))
+            )
+            .getInstance(LightBoardApplication.class)
+                .startApplication();
     }
 
 }
---
+++ src/main/java/net/amarantha/lightboard/module/SimulationModule.java
@@ -0,0 +1,22 @@
+package net.amarantha.lightboard.module;
+
+import com.google.inject.AbstractModule;
+import javafx.stage.Stage;
+import net.amarantha.lightboard.board.LightBoard;
+import net.amarantha.lightboard.board.impl.GraphicalBoard;
+
+public class SimulationModule extends AbstractModule {
+
+    private Stage primaryStage;
+
+    public SimulationModule(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    @Override
+    protected void configure() {
+        bind(LightBoard.class).to(GraphicalBoard.class);
+        bind(Stage.class).toInstance(primaryStage);
+    }
+
+}
